fee110bee99cc14120db944d31380cc96680f8d8
Jihoon Nam <mpq05@naver.com>
2021-08-19 00:47:49 +0900

    variables and vector
    변수와 벡터 표현

---
+++ tutorial/02_variables/variables.m
@@ -0,0 +1,22 @@
+a = 3;
+b = 2;
+
+c = a*b
+c = c + 1 % 다른 언어들처럼 이렇게 + 가능
+
+d = a / b
+
+val_1 = 10;
+val_2 = 5;
+val_3 = 15;
+
+% pi 같은 애들은 변수 이름으로 쓰지 않는게 일반적인 원칙
+val_10 = sin(pi) 
+
+% clear를 통해 변수 삭제 가능
+clear a 
+% clear 얘만 치면 전부 지움
+
+%whos 를 입력하면 변수들의 byte, class 등을 확인 가능
+whos val_1
+% whos 얘만 치면 전부 확인 가능
